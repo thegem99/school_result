0bd3cd0e112885410fdd8199b04549ce5b1b099b
thegem99 <cruxzux@gmail.com>
2026-04-01 17:22:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 app = Flask(__name__)
 
-API_URL = "https://resultapi.biharboardonline.org/result"
+API_URL = "https://examapi.biharboardonline.org/result"
 CACHE = {}
 SUBJECT_LIST = ["HINDI", "SANSKRIT", "MATHEMATICS", "SCIENCE", "SOCIAL SCIENCE", "ENGLISH"]
 
